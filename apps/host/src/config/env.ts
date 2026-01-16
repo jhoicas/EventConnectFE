9ae3dd90b50fa68240a4f7040263cde357b0d9a2
yoiner castillo <jhoicas@gmail.com>
2026-01-16 14:48:50 -0500

    fix: actualizar URL de API por defecto a Digital Ocean en producción

--- apps/host/src/config/env.ts
+++ apps/host/src/config/env.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5555/api/';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://eventconnect-api-8oih6.ondigitalocean.app/api/';
