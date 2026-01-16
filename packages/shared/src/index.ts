e49e4772fb2b10871a4232b6bbf827a43f862d1e
yoiner castillo <jhoicas@gmail.com>
2026-01-16 15:43:58 -0500

    fix: corregir variable de entorno API_BASE_URL en shared para usar NEXT_PUBLIC_API_BASE_URL

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5555/api';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://eventconnect-api-8oih6.ondigitalocean.app/api/';
 
 export const ROUTES = {
   LOGIN: '/login',
